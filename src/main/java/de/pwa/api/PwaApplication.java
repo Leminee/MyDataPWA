f2c7df99235a8a0bd2dd63c3f5b966dfb98db1c1
Lemine <lemine.el.agheb1994@gmail.com>
2022-03-19 04:22:44 +0100

    first commit

---
+++ src/main/java/de/pwa/api/PwaApplication.java
@@ -0,0 +1,13 @@
+package de.pwa.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/pwa/api/PwaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.pwa.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
